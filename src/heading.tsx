e45b21251c93113ca1ebab16fc2d31c3449a9f97
бону <qudratovamohtabonu@gmail.com>
2025-07-03 19:12:11 +0500

    added user branch

--- src/heading.tsx
+++
@@ -1,5 +0,0 @@
-const Heading = () => {
-	return <h1 className="text-4xl font-bold">Hello world</h1>;
-};
-
-export default Heading;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Heading from './heading';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-			<Heading />
+		
 	</StrictMode>
 );
